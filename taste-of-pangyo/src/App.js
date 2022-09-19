7d2b21280a4bff31876c34d71ad481249bd953ed
nahyunie <kongjui11@hansung.ac.kr>
2022-09-19 21:47:33 +0900

    naver map 관련 예제 소스

--- taste-of-pangyo/src/App.js
+++ taste-of-pangyo/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+
 
 function App() {
-  return (
-      <div className="App">
-        Hello World!
-      </div>
-  );
+    const mapElement = useRef(null);
+
+
+    useEffect(() => {
+        const { naver } = window;
+        if (!mapElement.current || !naver) return;
+
+        // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
+        const location = new naver.maps.LatLng(37.395, 127.1112);
+        const mapOptions: naver.maps.MapOptions = {
+            center: location,
+            zoom: 17,
+            zoomControl: true,
+            zoomControlOptions: {
+                position: naver.maps.Position.TOP_RIGHT,
+            },
+        };
+        const map = new naver.maps.Map(mapElement.current, mapOptions);
+        new naver.maps.Marker({
+            position: location,
+            map,
+        });
+    }, []);
+
+
+    return <div ref={mapElement} style={{ height: '100vh' }} />;
 }
+
 
 export default App;
